e9c8555d270541d8c51ea455d2697a3a65b2fab0
khandaniyal <daniyal.khan@iesjoandaustria.org>
2020-11-15 16:57:16 +0000

    currently working but needs dome tweaks

---
+++ accessUSB.java
@@ -0,0 +1,50 @@
+import java.awt.Desktop;
+import java.io.*;
+
+
+public class accessUSB{
+
+	public static void main(String[] args)throws IOException {
+		BufferedReader reader = new BufferedReader (new InputStreamReader (System.in));
+		
+		Console c = System.console();
+		
+
+		if(c == null) {
+			
+			System.out.println("exit ");
+			System.exit(1);
+		}
+		 
+		Desktop desktop = Desktop.getDesktop();
+	    File dirToOpen = null;
+		
+	 
+
+	    
+		String user = c.readLine("Username: ");
+		
+		char pass[] = c.readPassword("Enter password: ");
+		String uPass = new String(pass);
+
+		
+				
+		if(user.equals("yuto") && uPass.equals("abascalesgay")) {
+			
+			try {
+				dirToOpen = new File("E:\\encrypted");
+				desktop.open(dirToOpen);
+			}
+			
+			catch (IllegalArgumentException iae) {
+				System.out.println("File Not Found");
+			}
+				
+			}
+		else {
+			System.out.println("Credenciales no válidas we, vuelve a intentarlo.");
+		}
+	}
+
+}
+
